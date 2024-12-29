e796c08bd03d8d8ef1b5bdb149d5c9f5cb9688b7
NIGAM YADAV <71872897+nigamyadav72@users.noreply.github.com>
2024-12-29 08:18:27 +0545

    javascript added

--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,60 @@
-console.log("bye world")
-console.log("hahaha")
+// var cup = "coffee"
+// var collegebag= "book"
+// var total = cup + collegebag
+// // console.log(total)
+// var isNepali = true
+// var age = 22
+// console.log(cup)
+// console.log(age)
+// cup = 2
+// console.log(cup)// javascript is a dynamic language
+// console.log(typeof cup)
+
+// let cup = "coffee"
+// let collegebag= "book"
+// let total = cup + collegebag
+// // console.log(total)
+// let isNepali = true
+// let age = 22
+
+// const cup = "coffee"
+// const collegebag= "book"
+// const total = cup + collegebag
+// // console.log(total)
+// const isNepali = true
+// const age = 22
+
+var names = ["ram","shyam","hari"]//array declaration
+console.log(names.length)//here length is a property 
+names.push("sita")//push is a method that enter element at last of array
+console.log(names)
+names.pop()//pop is a method that removes last element from array
+console.log(names)
+names.shift()//shift is a method that removes first element from array
+console.log(names)
+names.unshift("sita")//unshift is a method that enter element at first of array
+console.log(names)
+names.reverse()//reverse is a method that reverse the array
+console.log(names)
+// console.log(names[0])
+// console.log(names[1])
+// console.log(names[2])
+
+
+//object
+
+// var person1 = {
+//     name: "nigam yadav",
+//     age : 22,
+//     address : "dharan"
+// }
+// person1.name = "manish basnet"
+
+// console.log(person1.name)
+// console.log(person1['age'])//different way to access object property
+// console.log(person1.name,person1.age,person1.address)//different way to access object property
+
+// console.log(person1)//key value change garna milxa
+// delete person1.age
+// console.log(person1)
+
